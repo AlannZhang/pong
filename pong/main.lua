6e1b3887c9b8f10987a94db3470fd75cbca67d86
AlannZhang <64608645+AlannZhang@users.noreply.github.com>
2020-08-22 13:38:12 -0400

    Update main.lua

--- pong/main.lua
+++ pong/main.lua
@@ -1,23 +1,38 @@
 -- Pong created in Lua using the love2D engine
 
-window_width = 1920 -- window width and height at 16:9
-window_height = 1080        
+-- window_width = 1920 -- window width and height at 16:9
+-- window_height = 1080        
 
 function love.load() -- runs at start of the game
-    love.graphics.setDefaultFilter("nearest", "nearest")
-    setupScreen(window_width, window_height,{
-        fullscreen = false
-        vsync = true
-        resizable = false
-    }
+    paddlespeed1 = 200
+    paddlespeed2 = 200   
+
+    paddle1y = 30
+    paddle2y = 500
 end
 
-function love.keypressed(key) -- press q to quit pong
-    if key == 'q' then
+function love.keypressed(key) -- press escape to quit pong
+    if key == 'escape' then
         love.event.quit()
     end
 end
 
+function love.update(dt)
+        if love.keyboard.isDown("w") then
+        if paddle1y > 10 then
+            paddle1y = paddle1y - paddlespeed1 * dt
+        end
+    elseif love.keyboard.isDown('s') then 
+        if paddle1y < 520 then
+            paddle1y = paddle1y + paddlespeed1 * dt  
+        end 
+    end
+
+    if love.keyboard.isDown("up") then 
+end
+
 function love.draw()
-    
+    love.graphics.circle('fill', 800/2, 600/2, 7, 7) --Ball
+    love.graphics.rectangle('fill', 35, paddle1y, 10, 70) --Paddle
+    love.graphics.rectangle('fill', 755, paddle2y, 10, 70) --Paddle
 end 
