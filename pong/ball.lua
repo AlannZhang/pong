c9f395cdb3ed97f026ffc106f6070f4e1a199c37
AlannZhang <64608645+AlannZhang@users.noreply.github.com>
2020-08-25 16:17:01 -0400

    Ball Update 1
    The ball can move now

--- pong/ball.lua
+++ pong/ball.lua
@@ -1,7 +1,8 @@
 Ball = Object:extend()
 
 function Ball:new(x, y, width, height) -- Ball parameters
-    self.speed = 100
+    self.speedX = math.random(2) == 1 and -100 or 100
+    self.speedY = math.random(2) == 1 and -300 or 300
     self.x = x
     self.y = y
     self.width = width
@@ -9,9 +10,19 @@ function Ball:new(x, y, width, height) -- Ball parameters
 end
 
 function Ball:update(dt)
+    self.y = self.y + self.speedY * dt
+    self.x = self.x + self.speedX * dt
+
+    if self.y <= 0 then
+        self.y = 0
+        self.speedY = -self.speedY
+    elseif self.y + self.height >= 600 then
+        self.y = 600 - self.height
+        self.speedY = -self.speedY
+    end
 
 end
 
 function Ball:draw() -- Ball shape
-    love.graphics.circle('fill', self.x, self.y, self.width, self.height) 
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height) 
 end
