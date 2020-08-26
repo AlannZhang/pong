dd782ee998155e692b90070a2aba051c7573c4e0
AlannZhang <64608645+AlannZhang@users.noreply.github.com>
2020-08-26 16:33:11 -0400

    Update 3
    Included ball collision against paddles from ball class and different game states.

--- pong/main.lua
+++ pong/main.lua
@@ -7,32 +7,66 @@
 ]]
 
 function love.load() 
-    Object = require "classic"
+    Object = require "classic" 
     require "ball"
     require "paddle1"
     require "paddle2"
 
-    b = Ball(400, 300, 7,7)
+    b = Ball(400, 300, 8,8)
     p1 = Paddle1(35, 30, 10, 70)
     p2 = Paddle2(755, 500, 10, 70)
 
     player1score = 0
     playerscore = 0
+
+    gameState = "start" -- Start screen game state
 end
 
 function love.keypressed(key) -- press escape to quit pong
     if key == 'escape' then
         love.event.quit()
     end
+
+    if key == "enter" or key == "return" then -- switch to player vs player mode
+        if gameState == "start" then
+            gameState = "player"
+        end
+    end
+
+    if key =="shift" then -- switch to play against the computer
+        if gameState == "start" then
+            gameState = "computer"
+        end
+    end
 end
 
-function love.update(dt)
-    p1:update(dt) -- Paddle 1 movement
-    p2:update(dt) -- Paddle 2 movement
+function love.update(dt) 
+    if gameState == "player" then
+        p1:update(dt) -- Paddle 1 movement
+        p2:update(dt) -- Paddle 2 movement
+        
+        b:update(dt) -- ball movement
+        b:collision(p1) -- ball deflection from paddle
+        b:collision(p2)
+
+
+    end
 end
 
 function love.draw()
-    b:draw()
-    p1:draw()
-    p2:draw()
+    if gameState == "start" then
+        love.graphics.print("PONG", 250, 85, nil, 8, 8)
+        love.graphics.print("Press enter or return for 2 Player Mode", 160, 250, nil, 2, 2)
+        love.graphics.print("Press shift to play against the computer", 155, 300, nil, 2, 2)
+    end
+
+    if gameState == "player" then
+        b:draw()
+        p1:draw()
+        p2:draw()
+    end
+
+    if gameState == "computer" then
+        
+    end
 end 
